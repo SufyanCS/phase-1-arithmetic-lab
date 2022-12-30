ff1db9459144a7a6868f8bb96abef1b4056c7b03
Sufyan Hussein <sufianb.contact@gmail.com>
2022-12-30 14:57:34 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 14;
+let num4 = 5;
+let multiply = num1 * num2;
+let random =parseInt(Math.random()*10);
+console.log(random)
+let mod = num3 % num4 ;
+let max = Math.max(16, 17, 18, 19, 20);
+
